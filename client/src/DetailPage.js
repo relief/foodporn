48b53f737a4f6b7bfbc07f82f75a2af47f82b542
Harry Xie <yxie@linkedin.com>
2017-06-29 19:30:01 -0700

    Put less contents in detail page to avoid scrolling

--- client/src/DetailPage.js
+++ client/src/DetailPage.js
@@ -13,22 +13,18 @@ class DetailPage extends Component {
                 <h2 className="title">H2 Title</h2>
                 <section>
                     <h3>H3 Heading</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute</p>
                     <p>
                         {this.props.img &&
-                          <img src={this.props.img.image_url} alt="Entrie"/>
+                          <img src={this.props.img.image_url}
+                               alt="Entrie"
+                               style={{width: '30vw'}}/>
                         }
                     </p>
                 </section>
                 <section>
-                    <h3>H3 Heading</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </p>
                 </section>
               </section>
               <Button onClick={e=>this.props.hideDetailPage()}>Close Popup</Button>
